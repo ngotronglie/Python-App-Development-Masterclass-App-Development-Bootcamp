1e1870d8fa2e991acbeef863eeb585dddcaa91a1
Liem <ngotrongliem2004@gmail.com>
2025-03-05 00:25:34 +0700

    Dừng

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from kivymd.app import MDApp
-# pip install kivymd
+from kivymd.uix.label import MDLabel, MDIcon
+
 
 class TrialApp(MDApp):
     def build(self):
-        return
+        label = MDLabel(text="Hey Everyone", halign="center", theme_text_color="Custom", text_color=(245/255, 100/255, 85/255), font_style="Caption")
+        icon_label = MDIcon(icon='language-python', halign="center")
+        return label  # Trả về label
+
 TrialApp().run()
